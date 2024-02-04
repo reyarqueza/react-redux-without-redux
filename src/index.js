55af9f73bc6bbeae99bddcfb7f98d8cb43939734
Rey Arqueza <reyarqueza@gmail.com>
2024-02-04 01:30:22 +0000

    updated to latest react and dependencies

--- src/index.js
+++ src/index.js
@@ -1,15 +1,52 @@
 import React, { useReducer, useContext, createContext } from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 
 const initialState = { x: 0, y: 0 };
 const SharedStateContext = createContext();
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "SET_X": {
+      return {
+        ...state,
+        x: action.x,
+      };
+    }
+
+    case "SET_Y": {
+      return {
+        ...state,
+        y: action.y,
+      };
+    }
+
+    default: {
+      return state;
+    }
+  }
+}
+
 function Display() {
-  return <h3>{/* {x} + {y} = {Number(x) + Number(y)} */}</h3>;
+  const { state } = useContext(SharedStateContext);
+  const { x, y } = state;
+
+  return (
+    <h3>
+      {x} + {y} = {Number(x) + Number(y)}
+    </h3>
+  );
 }
 
 function Slider(props) {
   const { label, id, legend } = props;
+  const { state, dispatch } = useContext(SharedStateContext);
+
+  function handleChange(e) {
+    dispatch({
+      type: `SET_${label}`,
+      [id]: e.target.value,
+    });
+  }
 
   return (
     <fieldset>
@@ -28,18 +65,26 @@ function Slider(props) {
           type="range"
           min={0}
           max={50}
+          onChange={handleChange}
+          value={state[id]}
         />
       </div>
-      <strong style={{ display: "block", textAlign: "center" }}></strong>
+      <strong style={{ display: "block", textAlign: "center" }}>
+        {state[id]}
+      </strong>
     </fieldset>
   );
 }
 
 function App() {
-  const sharedState = {};
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const sharedState = {
+    state,
+    dispatch,
+  };
 
   return (
-    <>
+    <SharedStateContext.Provider value={sharedState}>
       <h1>React Hooks useReducer, useContext and createContext</h1>
       <h2>Redux without Redux!</h2>
       <Display />
@@ -47,8 +92,12 @@ function App() {
       <Slider id="x" label="X" legend="First Operand" />
       <br />
       <Slider id="y" label="Y" legend="Second Operand" />
-    </>
+    </SharedStateContext.Provider>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(<App />);
+
